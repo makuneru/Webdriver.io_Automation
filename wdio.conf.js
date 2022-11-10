938c699fee53dd223e6e52bee61b593b2158050e
Macnel Cayanan <92564299+makuneru@users.noreply.github.com>
2022-11-10 10:47:27 +0800

    Update wdio.conf.js
    update

--- wdio.conf.js
+++ wdio.conf.js
@@ -56,6 +56,9 @@ exports.config = {
       //
       browserName: "chrome",
       acceptInsecureCerts: true,
+      chromeOptions: {
+        args: ['headless', 'disable-gpu'],
+      },
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
