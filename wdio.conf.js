4b2f9e17dbf84ec226ec8c4904142ce9747de16f
Mac Cayanan <macnel.cayanan@oracle.com>
2022-11-10 11:33:55 +0800

    update

--- wdio.conf.js
+++ wdio.conf.js
@@ -123,7 +123,7 @@ exports.config = {
   framework: "mocha",
   //
   // The number of times to retry the entire specfile when it fails as a whole
-  // specFileRetries: 1,
+  specFileRetries: 2,
   //
   // Delay in seconds between the spec file retry attempts
   // specFileRetriesDelay: 0,
