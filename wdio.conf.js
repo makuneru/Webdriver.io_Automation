0384247c5b2182a6151d72414941bff1d28978e8
Mac Cayanan <macnel.cayanan@oracle.com>
2022-11-10 11:15:49 +0800

    update config

--- wdio.conf.js
+++ wdio.conf.js
@@ -56,8 +56,8 @@ exports.config = {
       //
       browserName: "chrome",
       acceptInsecureCerts: true,
-      chromeOptions: {
-        args: ['headless', 'disable-gpu'],
+      "goog:chromeOptions": {
+        args: ["--headless", "user-agent=...", "--disable-gpu", "--window-size=1440,735"],
       },
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
